970217049cc5a195efa4ae3d6c28164e9e0d2b76
Roman Alikevich <roma.alikevich@mail.ru>
2020-10-17 20:26:43 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('');
+  let len = arr.length -1;
+  let result = '';
+  while (len !== -1) {
+      result += arr[len];
+      len--;
+  }
+  return Number.parseInt(result);
 }
